45bd4b826f4bb9ca4b572d078af3d150bd64004f
Lucía <lzurd18@gmail.com>
2026-04-14 13:10:17 +0200

    Refactor ProveedoresController for clarity and structure

--- src/Controllers/ProveedoresController.cs
+++ src/Controllers/ProveedoresController.cs
@@ -2,8 +2,7 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using SegundaAPI.DTOs.Clientes;
-using SegundaAPI.Models;
+using src.Models;
 using src.Data;
 using src.DTOs.Proveedores;
 
