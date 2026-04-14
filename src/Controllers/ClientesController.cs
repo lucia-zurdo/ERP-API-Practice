d8c52ffb7a0544a8533f50cb00f93462db1bf348
Lucía <lzurd18@gmail.com>
2026-04-14 13:10:33 +0200

    Refactor ClientesController for improved clarity

--- src/Controllers/ClientesController.cs
+++ src/Controllers/ClientesController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using SegundaAPI.Models;
+using src.Models;
 using src.Data;
 using src.DTOs.Clientes;
 
